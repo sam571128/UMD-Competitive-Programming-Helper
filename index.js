b2fef3ecd5209bf3d9f5203ffa6f903004a01683
sam571128 <samlee.571128@gmail.com>
2023-03-19 14:03:47 -0400

    Changed config.json to .env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Require the necessary discord.js classes
+require('dotenv').config()
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const token  = process.env.token;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
