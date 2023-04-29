710edccc505bdfdd6c129ff8cc2904aa5d7bdbf4
sam571128 <samlee.571128@gmail.com>
2023-04-29 19:52:14 -0400

    Added ASK

---
+++ commands/ask.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const { SlashCommandBuilder, EmbedBuilder  } = require('discord.js');
+
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+    data : new SlashCommandBuilder()
+         .setName('ask')
+         .setDescription('Chat with the bot')
+         .addStringOption(option =>
+			option
+				.setName('question')
+				.setDescription('Ask a question')),
+          
+    async execute(interaction) {
+        await interaction.deferReply();   
+        const question = interaction.options.getString('question') ?? " ";
+
+        if (question == " "){
+			await interaction.editReply('Please ask a question!')
+			return;
+        }
+
+        const messages = [];
+
+        messages.push({role: "user", content: "You are a assistant for Competitive Programming, your job is to answer the questions."});
+        
+        const completion1 = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+          });
+
+        const completion_text1 = completion1.data.choices[0].message.content;
+
+        messages.push({role: "assistant", content: completion_text1});
+
+        messages.push({role: "user", content: question});
+        
+		const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+          });
+
+        const completion_text = completion.data.choices[0].message.content;
+
+        await interaction.channel.send(completion_text);
+
+        await interaction.editReply('Finished!');
+        
+        await interaction.deleteReply();
+    },
+}
